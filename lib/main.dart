2781b9f980e6ec3b567d884e331fec6f7cd152ab
Hevesh <hevesh000@gmail.com>
2022-02-10 16:13:10 +0530

    🔥Initialised Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,39 @@
 import 'package:erx/screens/onboarding_screen.dart';
+import 'package:erx/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // transparent status bar
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ),
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: "eRx",
       debugShowCheckedModeBanner: false,
-      home: OnBoardingScreen(
-        pages: [],
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const OnBoardingScreen(
+              pages: [],
+            );
+          } else {
+            return const SplashScreen();
+          }
+        },
       ),
     );
   }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Text("splash"),
+      ),
+    );
+  }
+}
