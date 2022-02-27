8edfdb8f5a6a889767c95b93932e125d74b1000b
Hevesh <hevesh000@gmail.com>
2022-02-27 23:55:48 +0530

    🔐Login screen ui updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:erx/screens/otp_screen.dart';
+import 'package:erx/utils/color_palette.dart';
+import 'package:erx/widgets/background_stripes.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
@@ -8,28 +11,111 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(controller: _phoneNoController),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return OtpScreen(phoneNo: _phoneNoController.text);
-                    },
+      resizeToAvoidBottomInset: false,
+      backgroundColor: ColorPalette.charlestonGreen,
+      body: Stack(
+        children: [
+          const BackgroundStripes(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Expanded(
+                child: SizedBox(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20.0,
+                ),
+                child: Text(
+                  "Enter your phone number",
+                  style: GoogleFonts.nunito(
+                    fontSize: 25,
+                    color: ColorPalette.honeyDew,
                   ),
-                );
-              },
-              child: const Text("Submit"),
-            ),
-          ],
-        ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: ColorPalette.coolGrey,
+                      width: 2,
+                    ),
+                  ),
+                  height: 60,
+                  //TODO: Do Validation
+                  child: TextField(
+                    autofocus: true,
+                    textInputAction: TextInputAction.done,
+                    controller: _phoneNoController,
+                    keyboardType: TextInputType.phone,
+                    style: GoogleFonts.nunito(
+                      fontSize: 20,
+                      color: ColorPalette.honeyDew,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "00000 00000",
+                      hintStyle: GoogleFonts.nunito(
+                        fontSize: 20,
+                        color: const Color(0xff707070).withOpacity(0.2),
+                      ),
+                      prefixText: "+91 ",
+                      prefixStyle: GoogleFonts.nunito(
+                        fontSize: 20,
+                        color: ColorPalette.honeyDew.withOpacity(0.6),
+                      ),
+                      filled: true,
+                      fillColor: Colors.transparent,
+                    ),
+                    cursorColor: ColorPalette.honeyDew,
+                  ),
+                ),
+              ),
+              const Expanded(
+                child: SizedBox(),
+              ),
+              Center(
+                child: MaterialButton(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 8,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  color: ColorPalette.malachiteGreen,
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return OtpScreen(phoneNo: _phoneNoController.text);
+                        },
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Next",
+                    style: GoogleFonts.nunito(
+                      fontSize: 25,
+                      color: ColorPalette.chineseBlack,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/utils/color_palette.dart
+++ lib/utils/color_palette.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 mixin ColorPalette {
-  static const Color chineseBlack = Color(0xff111418);
-  static const Color charlestonGreen = Color(0xff222831);
-  static const Color coolGrey = Color(0xffAB9D87);
+  static const Color honeyDew = Color(0xffF8FFFA);
   static const Color malachiteGreenLight = Color(0xff72FD9E);
-  static const Color honeyDew = Color(0xffEBFFF1);
+  static const Color malachiteGreen = Color(0xff5ADD83);
+  static const Color coolGrey = Color(0xff8795AB);
+  static const Color charlestonGreen = Color(0xff222831);
+  static const Color chineseBlack = Color(0xff111418);
 }
