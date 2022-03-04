70a0c309ec4e5d930692a6759f26d253b7dce466
Hevesh <hevesh000@gmail.com>
2022-03-04 18:32:23 +0530

    🅰️Signup screen designed

--- lib/screens/patient_signup_screen.dart
+++ lib/screens/patient_signup_screen.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:erx/utils/color_palette.dart';
+import 'package:erx/widgets/background_stripes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PatientSignupScreen extends StatelessWidget {
   PatientSignupScreen({Key? key}) : super(key: key);
@@ -12,38 +13,100 @@ class PatientSignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(controller: _nameController),
-            ),
-            MaterialButton(
-              onPressed: () {
-                final String _userType = Provider.of<SharedPreferences>(
-                  context,
-                  listen: false,
-                ).getString("userType")!;
-
-                final String _phoneNo =
-                    FirebaseAuth.instance.currentUser!.phoneNumber!;
-                  // FirebaseFirestore.instance.collection(_userType).
-                FirebaseFirestore.instance
-                    .collection(_userType)
-                    .doc(_phoneNo)
-                    .set(
-                  {
-                    "name": _nameController.text,
-                    "phoneNo": _phoneNo,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: ColorPalette.charlestonGreen,
+      body: Stack(
+        children: [
+          const BackgroundStripes(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 75,
+              ),
+              const Expanded(
+                child: SizedBox(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20.0,
+                ),
+                child: Text(
+                  "Tell us your name",
+                  style: GoogleFonts.nunito(
+                    fontSize: 25,
+                    color: ColorPalette.honeyDew,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 60,
+                  //TODO: Do Validation
+                  child: TextField(
+                    textInputAction: TextInputAction.done,
+                    controller: _nameController,
+                    keyboardType: TextInputType.name,
+                    decoration: const InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: ColorPalette.honeyDew,
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                    style: GoogleFonts.nunito(
+                      fontSize: 20,
+                      color: ColorPalette.honeyDew,
+                    ),
+                    cursorColor: ColorPalette.honeyDew,
+                  ),
+                ),
+              ),
+              const Expanded(
+                child: SizedBox(),
+              ),
+              Center(
+                child: MaterialButton(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 8,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  color: ColorPalette.malachiteGreen,
+                  onPressed: () {
+                    final String _phoneNo =
+                        FirebaseAuth.instance.currentUser!.phoneNumber!;
+                    FirebaseFirestore.instance
+                        .collection("patient")
+                        .doc(_phoneNo)
+                        .set(
+                      {
+                        "name": _nameController.text,
+                        "phoneNo": _phoneNo,
+                      },
+                    );
                   },
-                );
-              },
-              child: const Text("Submit"),
-            ),
-          ],
-        ),
+                  child: Text(
+                    "Next",
+                    style: GoogleFonts.nunito(
+                      fontSize: 25,
+                      color: ColorPalette.chineseBlack,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 75,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
