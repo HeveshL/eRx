95dd5856e7dda14de2f20762e994a63324a8cc9d
Hevesh <hevesh000@gmail.com>
2022-03-10 20:13:57 +0530

    🏠Home screen top part done

--- lib/screens/temp_home_screen.dart
+++ lib/screens/temp_home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:erx/utils/color_palette.dart';
+import 'package:erx/utils/svg_strings.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,14 +13,140 @@ class TempHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: IconButton(
-          icon: const Icon(Icons.power_settings_new_outlined),
-          onPressed: () {
-            Provider.of<SharedPreferences>(context,listen: false).remove("userType");
-            FirebaseAuth.instance.signOut();
-          },
-        ),
+      backgroundColor: ColorPalette.charlestonGreen,
+      body: Stack(
+        children: [
+          Expanded(
+            child: SvgPicture.string(SvgStrings.homeScreenBackground),
+          ),
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 30,
+                        width: 30,
+                        color: Colors.blue,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Transform.rotate(
+                                angle: 8 * 22 / 7 / 180,
+                                child: SvgPicture.string(
+                                  SvgStrings.logoE,
+                                  height: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Material(
+                            color: Colors.transparent,
+                            child: Text(
+                              "prescription",
+                              style: GoogleFonts.nunito(
+                                color: ColorPalette.honeyDew,
+                                fontSize: 23,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        height: 30,
+                        width: 30,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30, left: 20),
+                  child: Text(
+                    "Hello John!",
+                    style: GoogleFonts.nunito(
+                      color: ColorPalette.honeyDew,
+                      fontSize: 23,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 5, left: 20),
+                  child: Text(
+                    "Hope you have a great day!",
+                    style: GoogleFonts.nunito(
+                      color: ColorPalette.honeyDew,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 17.0),
+                        child: Container(
+                          height: double.infinity,
+                          width: double.infinity,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(34),
+                              topRight: Radius.circular(34),
+                            ),
+                            color: ColorPalette.chineseBlack,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 40,
+                          right: 40,
+                        ),
+                        child: Container(
+                          height: 40,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: IconButton(
+              icon: const Icon(Icons.power_settings_new_outlined),
+              color: Colors.white,
+              onPressed: () {
+                Provider.of<SharedPreferences>(
+                  context,
+                  listen: false,
+                ).remove("userType");
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
